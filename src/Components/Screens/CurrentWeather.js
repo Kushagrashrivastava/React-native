22029458aef4f5eb22956db73e9e2e64af1da08c
Kushagra_Shrivastava <kushagra10june@gmail.com>
2023-08-24 23:39:38 +0530

    Static part completed

--- src/Components/Screens/CurrentWeather.js
+++ src/Components/Screens/CurrentWeather.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 import RowText from "../RowText";
+import { weatherType } from "../../Utilities/weatherType";
 
 const CurrentWeather = () => {
   const { wrapper, container, temp, feels, highLowWrapper, highLow, bodyWrapper, description, message } = styles
@@ -21,7 +22,7 @@ const CurrentWeather = () => {
       </View>
       <RowText 
       messageOne={'Its sunny'}
-      messageTwo={'Its perfect t-shirt weather'}
+      messageTwo={weatherType['Thunderstorm'].message}
       containerStyles={bodyWrapper}
       messageOneStyles={description}
       messageTwoStyles={message}
